1d6748b9abdaad14ec30a6d5818c13c17ff47b94
prin <hi@prin.studio>
2023-10-16 17:15:42 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# just-pnpm
+
+This package utilizes npm package's `preinstall` and `postinstall` [scrips](https://docs.npmjs.com/cli/v9/using-npm/scripts) to restrict that only [pnpm](https://pnpm.io/) package manager could be used on a project.
+
+It's developed as an alternative to `npx only-allow pnpm`, with some bugs fixed ([#15](https://github.com/pnpm/only-allow/issues/15), [#2660](https://github.com/npm/cli/issues/2660)).
+
+## Installation
+
+```
+pnpm add -D just-pnpm
+```
+
+## Usage
+
+Add this package to your project's `devDependencies` and that's it.
+
+If someone executes package installation commands other than pnpm (`npm install`, `yarn`, `cnpm install`, whatever.) on the project, they will get an error instead and installation will not proceed.
+
+```json
+{
+  "name": "your-project",
+  "devDependencies": {
+    "just-pnpm": "^0.0.1"
+  }
+}
+```
+
+For library developers, adding `just-pnpm` to your package's `dependencies` will prevent your library from being installed by any package managers except for pnpm. (not recommended though)
+
+## Escape Hatches
+
+Set environment variable `JUST_PNPM_SKIP_CHECK=true` to disable the check temporarily.
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+/**
+ * Credit: https://www.npmjs.com/package/which-pm-runs
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2017-2022 Zoltan Kochan <z@kochan.io>
+ * https://github.com/zkochan/packages/blob/main/which-pm-runs/LICENSE
+ */
+function whichPMRuns() {
+  const userAgent = process.env.npm_config_user_agent;
+  if (!userAgent) {
+    return undefined;
+  }
+
+  const pmSpec = userAgent.split(' ')[0];
+  const separatorPos = pmSpec.lastIndexOf('/');
+  const name = pmSpec.substring(0, separatorPos);
+  return {
+    name: name === 'npminstall' ? 'cnpm' : name,
+    version: pmSpec.substring(separatorPos + 1),
+  };
+}
+
+function isEnv(name) {
+  return typeof process.env[name] !== 'undefined';
+}
+
+const wantedPM = 'pnpm';
+const usedPM = whichPMRuns();
+const shouldSkip = isEnv('JUST_PNPM_SKIP_CHECK');
+
+if (isEnv('JUST_PNPM_DEBUG')) {
+  console.log({ wantedPM, usedPM, shouldSkip });
+}
+
+// Credit: https://github.com/pnpm/only-allow/blob/master/bin.js
+// with external dependencies removed, package manager restricted to pnpm,
+// and isInstalledAsDependency check removed.
+if (usedPM && usedPM.name !== wantedPM && !shouldSkip) {
+  console.log(
+    `Use "pnpm install" for installation in this project.
+
+If you don't have pnpm, install it via "npm i -g pnpm".
+For more details, go to https://pnpm.js.org/`
+  );
+  process.exit(1);
+}
